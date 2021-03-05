412ecaf34b3f085119bd43cdbd9d49a6b5ed0c0d
Brian Rocca <brocca@iu.edu>
2021-03-05 14:13:45 -0500

    added more comments to Maxmatch

--- 01_Maxmatch/maxmatch.py
+++ 01_Maxmatch/maxmatch.py
@@ -1,15 +1,17 @@
 import sys
-
+#Create function "Maxmatch"
 def maxmatch(sentence, dictionary):
+	#if sentence is empty, return empty list
 	if sentence == '':
 		return []
+	#Try to find the longest match possible in dictionary
 	for i in range(0, len(sentence)):
 		firstword = sentence[0:-i]
 		remainder = sentence[-i:]
 		#if there's a match in the dictionary, print
 		if firstword in dictionary:
 			return [firstword] + maxmatch(remainder, dictionary)
-	#if firstword not in dictionary:
+	#if firstword not in dictionary,print first character of line
 	firstword = sentence[0]
 	remainder = sentence[1:]
 	return [firstword] + maxmatch(remainder, dictionary)
